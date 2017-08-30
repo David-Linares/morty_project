8e8c07189e40fd3bc5e700c9da4275a3c7cb7f80
David Linares <dlinares@localhost.localdomain>
2017-08-30 03:30:03 -0500

    Cambios locales, ingreso de múltiples funciones y audio múltiple

--- main/views.py
+++ main/views.py
@@ -15,34 +15,45 @@ def index(request):
     # Si llega el método post.
     if request.POST:
         if request.POST.get('latex_form', False): # Si llega post para convertir
-            name_record = "last_record.mp3"
-            final_name = "last_record.ogg"
-            # Convierte la ecuación latex a mathml
-            mathml_output = latex2mathml.converter.convert(request.POST['latex_form'])
-            matches_list = find_matches(request.POST['latex_form'])
-            tts_str = ""
-            if len(matches_list) > 0:
-                for m in matches_list:
-                    tts_str += search_string_db(m)
-            else:
-                tts_str = request.POST['latex_form']
-            print(tts_str)
-            print('racc' in tts_str)
-            if 'frac' in request.POST['latex_form']:
-                tts_str = tts_str.replace('}{', ' sobre ')
-            print(tts_str)
-            # Genera el código para generar el audio de lectura.
-            text = gTTS(text=tts_str, lang='es')
-            # Borra los archivos anteriores
-            try:
-                os.remove("main/static/"+name_record)
-            except:
-                pass
-            #Obtiene fecha para el nombre del archivo
-            text.save("main/static/"+name_record)
-            # Convierte el audio
-            convert_audio(name_record, final_name)
-            return render(request, 'main/index.html', {"mathml_data": mathml_output, "latex_form": request.POST['latex_form'], "name_record": final_name})
+            name_record = "last_record%d.mp3"
+            final_name = "last_record%d.ogg"
+            list_final = []
+            mathml_output = []
+            # Listado de ecuaciones escritas.
+            equa_list = request.POST['latex_form'].split('\n')
+            print(equa_list)
+            for index, value in enumerate(equa_list):
+                # Convierte la ecuación latex a mathml
+                mathml_output.append(latex2mathml.converter.convert(value))
+                matches_list = find_matches(value)
+                tts_str = ""
+                if len(matches_list) > 0:
+                    for m in matches_list:
+                        tts_str += search_string_db(m)
+                else:
+                    tts_str = value
+                print(tts_str)
+                print('racc' in tts_str)
+                if 'frac' in value:
+                    tts_str = tts_str.replace('}{', ' sobre ')
+                print(tts_str)
+                # Genera el código para generar el audio de lectura.
+                text = gTTS(text=tts_str, lang='es')
+                # Borra los archivos anteriores
+                try:
+                    os.remove("main/static/" + name_record % index)
+                except:
+                    pass
+                # Obtiene fecha para el nombre del archivo
+                text.save("main/static/" + name_record % index)
+                # Convierte el audio
+                convert_audio(name_record % index, final_name % index)
+                list_final.append(final_name % index)
+            print(mathml_output)
+            print(list_final)
+            return render(request, 'main/index.html',
+                              {"mathml_data": mathml_output, "latex_form": request.POST['latex_form'],
+                               "name_record": list_final})
     else:
         print("GET METHOD")
         return render(request, 'main/index.html', {})
